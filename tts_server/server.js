7de5405dc7d538bcec979037d02a8871aed8ae28
Daphne Chu <daphnechu28@gmail.com>
2026-02-21 23:53:17 +0800

    Changed callbackService to a get route instead

--- tts_server/server.js
+++ tts_server/server.js
@@ -4,11 +4,12 @@ const express = require("express")
 const bodyParser = require("body-parser")
 // Importing the ttsRoute module, which likely contains the route handlers for text-to-speech functionality.
 const ttsRoute = require("./routes/ttsRoute")
+const audioRoute = require("./routes/audioRoute")
 
 const app = express()
 app.use(bodyParser.json())
 
 // Mount the ttsRoute on the "/api" path, meaning that any requests to "/api" will be handled by the ttsRoute module.
 app.use("/api", ttsRoute)
-
+app.use("/api", audioRoute)
 app.listen(3000, () => console.log("Server running on port 3000"))
